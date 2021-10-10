d2550d4fde695dd50257f9252e9e9dd04d32d4e3
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-10 12:36:51 -0700

    added helper functions to eval

--- src/commands/util/EvalCommand.ts
+++ src/commands/util/EvalCommand.ts
@@ -1,6 +1,7 @@
 import { Message } from 'discord.js';
 import BaseCommand from '../../utils/structures/BaseCommand';
 import DiscordClient from '../../client/client';
+import { addCoinsToTotal, getTotalCoins } from '../../utils/user';
 
 export default class EvalCommand extends BaseCommand {
   constructor() {
@@ -16,7 +17,11 @@ export default class EvalCommand extends BaseCommand {
   async run(client: DiscordClient, message: Message, args: Array<string>) {
     if (message.author.id === '696554549418262548') {
       try {
+        const evalAddCoinsToTotal = addCoinsToTotal;
+        const evalGetTotalCoins = getTotalCoins;
+
         eval(message.content.substring(7));
+        message.reply('success');
       } catch (err) {
         message.reply('you messed up your code:\n' + err);
       }
@@ -26,7 +31,7 @@ export default class EvalCommand extends BaseCommand {
           {
             title: "You can't use this command.",
             description:
-              'This command is so dangerous (it could litterally wipe all files of the server this bot is running on) that only the creator of the bot can use it.',
+              'This command is so dangerous (it could literally wipe all files of the server this bot is running on) that only the creator of the bot can use it.',
           },
         ],
       });
