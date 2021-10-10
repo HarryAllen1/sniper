26bacb77aeb15079f46004d593f989d89af6fd92
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-10 12:36:58 -0700

    added ping command

---
+++ src/commands/util/PingCommand.ts
@@ -0,0 +1,61 @@
+import { Message } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/client';
+import admin from 'firebase-admin';
+import { getUserData } from '../../utils/user';
+import { msToTime } from '../../utils/date';
+
+const db = admin.firestore();
+
+export default class PingCommand extends BaseCommand {
+  constructor() {
+    super(
+      'ping',
+      'util',
+      [],
+      5000,
+      'Pings the bot and returns response times.'
+    );
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    message
+      .reply({ embeds: [{ title: 'pinging....', color: 'RED' }] })
+      .then((msg) => {
+        const messageSendTime = Date.now();
+        let dbPing: number;
+        getUserData(message.author.id).then(() => {
+          dbPing = Date.now();
+          msg.edit({
+            embeds: [
+              {
+                title: 'Results',
+                description: 'these numbers do *not* represent lag.',
+                color: 'GREEN',
+                fields: [
+                  {
+                    name: 'Latency (Date.now() - message.createdTimestamp)',
+                    value: `${
+                      msg.createdTimestamp - message.createdTimestamp
+                    }ms`,
+                  },
+                  {
+                    name: 'API Latency (client.ws.ping)',
+                    value: `${client.ws.ping.toString()}ms`,
+                  },
+                  {
+                    name: 'Database latency (affects currency commands)',
+                    value: `${(dbPing! - messageSendTime).toString()}ms`,
+                  },
+                  {
+                    name: 'Uptime (how long since the last bot restart; affects `snipe` commands)',
+                    value: `${msToTime(client.uptime!)}`,
+                  },
+                ],
+              },
+            ],
+          });
+        });
+      });
+  }
+}
