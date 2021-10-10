206602bd6a8e7e0f55f5d8e799c37936dcb0a4f1
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-10 12:35:02 -0700

    added everything to try-catch

--- src/sniper.ts
+++ src/sniper.ts
@@ -4,24 +4,28 @@ import DiscordClient from './client/client';
 import { Intents } from 'discord.js';
 import admin from 'firebase-admin';
 
-const client = new DiscordClient({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_PRESENCES,
-  ],
-});
+try {
+  const client = new DiscordClient({
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_MEMBERS,
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      Intents.FLAGS.GUILD_PRESENCES,
+    ],
+  });
 
-admin.initializeApp({
-  credential: admin.credential.cert(require('../firebase-credental.json')),
-  projectId: 'discord-sniper-5c7f0',
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(require('../firebase-credental.json')),
+    projectId: 'discord-sniper-5c7f0',
+  });
 
-(async () => {
-  client.prefix = config.prefix || client.prefix;
-  await registerCommands(client, '../commands');
-  await registerEvents(client, '../events');
-  await client.login(config.token);
-})();
+  (async () => {
+    client.prefix = config.prefix || client.prefix;
+    await registerCommands(client, '../commands');
+    await registerEvents(client, '../events');
+    await client.login(config.token);
+  })();
+} catch (error) {
+  console.error(error);
+}
