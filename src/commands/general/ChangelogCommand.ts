0a2391347bcd8a0301df287c26b1c1c25f0cd9e1
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-11 16:45:26 -0700

    added changelog command

---
+++ src/commands/general/ChangelogCommand.ts
@@ -0,0 +1,42 @@
+import { Message } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/client';
+import Axios from 'axios';
+import { GithubCommits } from '../../utils/typings/Github';
+import { reply } from '../../utils/helpers/reply';
+
+export default class ChangelogCommand extends BaseCommand {
+  constructor() {
+    super(
+      'changelog',
+      'general',
+      [],
+      5000,
+      'Shows changes made and plans for the bot.',
+      'this cooldown is to prevent spam.'
+    );
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    Axios.get<GithubCommits>(
+      'https://api.github.com/repos/MajesticString/sniper/commits?per_page=5'
+    ).then((res) => {
+      const { data } = res;
+      const toReadableDate = (date: string) => {
+        return new Date(date).toUTCString();
+      };
+      let githubCommitMessages: any[] = [];
+      data.forEach((commit) => {
+        githubCommitMessages.push({
+          name: toReadableDate(commit.commit.author.date),
+          value: commit.commit.message,
+        });
+      });
+      reply(message, {
+        title: 'Changelog',
+        fields: githubCommitMessages,
+        color: 'WHITE',
+      });
+    });
+  }
+}
