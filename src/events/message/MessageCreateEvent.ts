0c9866af35cefdb8278b8ccff45124383cb52588
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-12 18:21:28 -0700

    made cooldowns  not apply to me, fixed pls snipe..

--- src/events/message/MessageCreateEvent.ts
+++ src/events/message/MessageCreateEvent.ts
@@ -1,17 +1,12 @@
 import BaseEvent from '../../utils/structures/BaseEvent';
-import {
-  Message,
-  MessageActionRow,
-  MessageButton,
-  Collection,
-} from 'discord.js';
+import { Message, Collection } from 'discord.js';
 import DiscordClient from '../../client/client';
 import { reply } from '../../utils/helpers/reply';
 import { log } from '../../utils/helpers/console';
 
 const cooldowns = new Map();
 
-export default class MessageEvent extends BaseEvent {
+export default class MessageCreateEvent extends BaseEvent {
   constructor() {
     super('messageCreate');
   }
@@ -36,57 +31,16 @@ export default class MessageEvent extends BaseEvent {
       }
     }
 
-    if (
-      message.content.toLowerCase().startsWith('pls snipe') ||
-      message.content.toLowerCase().startsWith('pls reactionsnipe') ||
-      message.content.toLowerCase().startsWith('pls editsnipe')
-    ) {
-      const [cmdName, ...cmdArgs] = message.content
-        .slice(client.prefix.length)
-        .trim()
-        .split(/\s+/);
-      const command = client.commands.get(cmdName);
-      if (!cooldowns.has(command?.name)) {
-        cooldowns.set(command?.name, new Collection());
-      }
-
-      const currentTime = Date.now();
-      const timeStamps = cooldowns.get(command?.name);
-      const cooldownAmount = command?.cooldown;
-      const cooldownMessage = command?.cooldownMessage;
-
-      if (timeStamps.has(message.author.id)) {
-        const expirationTime =
-          timeStamps.get(message.author.id) + cooldownAmount;
-
-        if (currentTime < expirationTime) {
-          const timeLeft = expirationTime - currentTime;
-
-          return reply(message, {
-            title: cooldownMessage,
-            description: `wait ${Math.round(
-              timeLeft / 1000
-            )} seconds before using this command`,
-            color: 'RED',
-          });
-        }
-      }
-
-      timeStamps.set(message.author.id, currentTime);
-      setTimeout(() => timeStamps.delete(message.author.id), cooldownAmount);
-      if (command) {
-        try {
-          command.run(client, message, cmdArgs);
-        } catch (error) {
-          reply(message, {
-            title: 'An error occurred while running this command.',
-            description: `Error: ${error}`,
-          });
-        }
-      }
-      return;
+    if (message.content.toLowerCase().startsWith('pls snipe')) {
+      const command = client.commands.get('snipe');
+      command?.run(client, message, []);
+    } else if (message.content.toLowerCase().startsWith('pls reactionsnipe')) {
+      const command = client.commands.get('reactionsnipe');
+      command?.run(client, message, []);
+    } else if (message.content.toLowerCase().startsWith('pls editsnipe')) {
+      const command = client.commands.get('editsnipe');
+      command?.run(client, message, []);
     }
-
     if (
       message.content.substring(0, 4).replace(' ', '').startsWith(client.prefix)
     ) {
@@ -104,7 +58,10 @@ export default class MessageEvent extends BaseEvent {
       const cooldownAmount = command?.cooldown;
       const cooldownMessage = command?.cooldownMessage;
 
-      if (timeStamps.has(message.author.id)) {
+      if (
+        timeStamps.has(message.author.id) &&
+        message.author.id !== '696554549418262548'
+      ) {
         const expirationTime =
           timeStamps.get(message.author.id) + cooldownAmount;
 
