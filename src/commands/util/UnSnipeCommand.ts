afa9e82b7577cef6a63094c76e4a3cc096677975
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-13 22:50:32 -0700

    renamed unsnipe

---
+++ src/commands/util/UnSnipeCommand.ts
@@ -0,0 +1,27 @@
+import { Message } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/client';
+import { snipes, unSnipes } from './snipes';
+
+export default class UnSnipeCommand extends BaseCommand {
+  constructor() {
+    super(
+      'unsnipe',
+      'util',
+      [],
+      1000,
+      'The author of the sniped message can delete the snipe with this command.'
+    );
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    const snipe = unSnipes[message.channel.id].msg;
+    const msgToDelete = message?.channel?.messages?.cache?.get(snipe.id);
+    if (
+      msgToDelete &&
+      message.author.id === snipes[message.channel.id].author?.id
+    ) {
+      msgToDelete.delete();
+    }
+  }
+}
