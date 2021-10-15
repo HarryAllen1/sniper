b96ea475db5ee2b2b057365d70f20ccbd6fc1d4d
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-14 20:05:09 -0700

    cont. work on jailbreak cmd

--- src/commands/currency/JailBreakCommand.ts
+++ src/commands/currency/JailBreakCommand.ts
@@ -1,6 +1,9 @@
 import { Message } from 'discord.js';
 import BaseCommand from '../../utils/structures/BaseCommand';
 import DiscordClient from '../../client/client';
+import { getUserData, getUserDataRef } from '../../utils/helpers/user';
+import { reply } from '../../utils/helpers/reply';
+import randomNumber from '../../utils/helpers/randomNumber';
 
 export default class JailBreakCommand extends BaseCommand {
   constructor() {
@@ -17,6 +20,22 @@ export default class JailBreakCommand extends BaseCommand {
   }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-    message.channel.send('not implemented yet');
+    const userData = await getUserData(message.author.id);
+    if (!userData.inJailUntil) {
+      reply(message, {
+        title: 'You can only use this command when you are in jail!',
+        description:
+          'You are in jail when you commit so many crimes that your balance drops below -5000',
+        color: 'RED',
+      });
+      return;
+    }
+    const chanceOfSuccess = randomNumber(0, 75, true);
+    reply(message, {
+      title: `Your chance of success in this jailbreak is ${chanceOfSuccess}%`,
+      description:
+        'If you fail, you will lose 10k coins and you will be put in jail for an extra 2 hours. You can use this command in 15 minutes.',
+      color: 'LUMINOUS_VIVID_PINK',
+    });
   }
 }
