64d82a1d4cb38ca8a81dcd7cd4c185d3ddff54d6
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-10-26 21:44:52 -0700

    added more detail to ban event

--- src/events/guild/GuildBanAddEvent.ts
+++ src/events/guild/GuildBanAddEvent.ts
@@ -1,5 +1,5 @@
 // https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildBanAdd
-import { GuildBan } from 'discord.js';
+import { GuildBan, User } from 'discord.js';
 import BaseEvent from '../../utils/structures/BaseEvent';
 import DiscordClient from '../../client/client';
 import { sendMessageInBorderSecurity } from '../../utils/helpers/cambridge-server';
@@ -11,14 +11,51 @@ export default class GuildBanAddEvent extends BaseEvent {
 
   async run(client: DiscordClient, ban: GuildBan) {
     if (ban.guild.id !== '882695828140073052') return;
-    sendMessageInBorderSecurity(client, {
-      embeds: [
-        {
-          title: `${ban.user.tag} was banned`,
-          description: `Reason: ${ban.reason ? ban.reason : 'No reason given'}`,
-          color: 'RED',
-        },
-      ],
+    const fetchedLogs = await ban.guild.fetchAuditLogs({
+      limit: 1,
+      type: 'MEMBER_BAN_ADD',
     });
+    const banLog = fetchedLogs.entries.first();
+
+    if (!banLog) {
+      sendMessageInBorderSecurity(client, {
+        embeds: [
+          {
+            title: `${ban.user.tag} was banned`,
+            description: `Reason: ${
+              ban.reason ? ban.reason : 'No reason given'
+            }`,
+            color: 'RED',
+          },
+        ],
+      });
+      return;
+    }
+    const { executor, target } = banLog;
+    if ((target as User).id && (target as User).id === ban.user.id) {
+      sendMessageInBorderSecurity(client, {
+        embeds: [
+          {
+            title: `${ban.user.tag} was banned`,
+            description: `Reason: ${
+              ban.reason ? ban.reason : 'No reason given'
+            }\nExecutor: ${executor!.tag}`,
+            color: 'RED',
+          },
+        ],
+      });
+    } else {
+      sendMessageInBorderSecurity(client, {
+        embeds: [
+          {
+            title: `${ban.user.tag} was banned`,
+            description: `Reason: ${
+              ban.reason ? ban.reason : 'No reason given'
+            }`,
+            color: 'RED',
+          },
+        ],
+      });
+    }
   }
 }
