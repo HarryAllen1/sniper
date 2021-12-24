bc73525354f70d9166942b4f527cbc21df9cd445
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-12-24 10:39:12 -0800

    try to fix double sending

--- start.js
+++ start.js
@@ -1,12 +1,12 @@
 import fs from 'fs';
 
-if (!fs.existsSync('./node_modules')) $`npm install`;
 if (!fs.existsSync('./out-esm/src')) {
   $`npm run build`;
 }
 import { $ } from 'zx';
 
 import { main } from './out-esm/src/sniper.js';
+
 process.stdin.resume();
 const start = async () => {
   try {
@@ -20,8 +20,8 @@ start();
 process.on('uncaughtException', (a, b) => {
   console.log(
     'HAHAHHAHAHAHAHAHAHAHHAH the bot crashed',
-    `Error code: ${a.name}: ${a.message}`,
+    `Error code: ${a.name}: ${a.message}\n`,
     `idk: ${b}`
   );
-  $`node start.js`;
+  // $`node start.js`;
 });
