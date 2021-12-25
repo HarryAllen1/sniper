175a23c9541567e2e855f75ef6e9b3f138816459
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-12-25 14:47:15 -0800

    make all references to the date function just use the ms package

--- src/utils/helpers/date.ts
+++ src/utils/helpers/date.ts
@@ -1,11 +1,3 @@
-export const msToTime = (duration: number) => {
-  let seconds: string | number = Math.floor((duration / 1000) % 60),
-    minutes: string | number = Math.floor((duration / (1000 * 60)) % 60),
-    hours: string | number = Math.floor((duration / (1000 * 60 * 60)) % 24);
+import ms from "ms";
 
-  hours = hours < 10 ? '0' + hours : hours;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-
-  return hours + 'h, ' + minutes + 'm, ' + seconds + 's';
-};
+export const msToTime = ms;
