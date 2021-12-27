85b34abc4384e85b10e8c7b1385856eac2cd8131
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-12-26 17:01:07 -0800

    fix stuff

--- src/commands/giveaways/GEndCommand.ts
+++ src/commands/giveaways/GEndCommand.ts
@@ -1,10 +1,20 @@
 import { Message } from 'discord.js';
-import BaseCommand from '../../utils/structures/BaseCommand';
-import DiscordClient from '../../client/client';
+import BaseCommand from '../../utils/structures/BaseCommand.js';
+import DiscordClient from '../../client/client.js';
 
 export default class GEndCommand extends BaseCommand {
   constructor() {
-    super('gEnd', 'giveaways', []);
+    super(
+      'gEnd',
+      'giveaways',
+      [],
+      0,
+      "Ends a giveaway. You will need to obtain a message ID. If you don't know how to obtain a message ID, refer to this article: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-",
+      {
+        argsRequired: true,
+        argsDescription: '<message id>',
+      }
+    );
   }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
