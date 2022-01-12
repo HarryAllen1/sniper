f6e3f42a89b7a42166d034dd79dee42e3af3a022
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-01-12 12:03:34 -0800

    fix sudo command

--- src/commands/restricted/SudoCommand.ts
+++ src/commands/restricted/SudoCommand.ts
@@ -4,15 +4,15 @@ import DiscordClient from '../../client/client.js';
 
 export default class SudoCommand extends BaseCommand {
   constructor() {
-    super('sudo', 'restricted', [], 0, '', {
+    super('sudo', 'restricted', [], 0, 'Run a command as another user', {
       argsRequired: true,
       argsDescription: '<userID> <command> [...args]',
     });
   }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-    message.author === client.users.cache.get(args[0]);
-    message.member === message.guild?.members.cache.get(args[0]);
+    message.author = await client.users.fetch(args[0]);
+
     client.commands.get(args[1])?.run(client, message, args.slice(2));
   }
 }
