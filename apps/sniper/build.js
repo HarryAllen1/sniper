1ed9a8aaa8402775f59ef3debd5e9533e045bbb6
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-01-21 17:08:09 -0800

    SWC is the build tool now (better suited for my needs)

--- apps/sniper/build.js
+++
@@ -1,9 +0,0 @@
-import glob from 'glob';
-
-glob('src/**/*.ts', {}, async (err, files) => {
-  (await import('esbuild')).buildSync({
-    platform: 'node',
-    outdir: 'out',
-    entryPoints: files,
-  });
-});
