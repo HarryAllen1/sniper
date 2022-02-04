de21202f3fca1074c8e6afc5643476702101d489
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-02-03 16:40:44 -0800

    Fix docs update, fix gh actions

--- apps/sniper/src/utils/registry.ts
+++ apps/sniper/src/utils/registry.ts
@@ -1,18 +1,18 @@
-import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v9";
-import { Collection } from "discord.js";
-import { default as ms } from "ms";
-import { promises as fs } from "node:fs";
-import path from "node:path";
-import DiscordClient from "../client/client.js";
-import BaseCommand from "./structures/BaseCommand.js";
-import BaseEvent from "./structures/BaseEvent.js";
+import { RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types/v9';
+import { Collection } from 'discord.js';
+import { default as ms } from 'ms';
+import { promises as fs } from 'node:fs';
+import path from 'node:path';
+import DiscordClient from '../client/client.js';
+import BaseCommand from './structures/BaseCommand.js';
+import BaseEvent from './structures/BaseEvent.js';
 
 // interface CommandHelper {
 //   [name: string]: CommandCategory;
 // }
 
 const allCommandsJSON = JSON.parse(
-  fs.readFileSync("./all-commands.json", "utf-8")
+  await fs.readFile('./all-commands.json', 'utf-8')
 );
 
 interface CommandCategory {
@@ -31,7 +31,7 @@ export const helpCommandHelperCollection = new Collection<
 
 export const interactions: RESTPostAPIApplicationCommandsJSONBody[] = [];
 
-export async function registerCommands(client: DiscordClient, dir = "") {
+export async function registerCommands(client: DiscordClient, dir = '') {
   const filePath = path.join(process.cwd(), dir);
   const files = await fs.readdir(filePath);
 
@@ -43,8 +43,8 @@ export async function registerCommands(client: DiscordClient, dir = "") {
       // helpCommandHelper[file] = { commands: [] };
       helpCommandHelperCollection.set(file, { commands: [] });
     }
-    if (file.endsWith("Command.js") || file.endsWith("Command.ts")) {
-      const Command = (await import("../../" + path.join(dir, file))).default;
+    if (file.endsWith('Command.js') || file.endsWith('Command.ts')) {
+      const Command = (await import('../../' + path.join(dir, file))).default;
       const command = new Command() as BaseCommand;
 
       // if (!command.permissionsRequired) {
@@ -54,7 +54,7 @@ export async function registerCommands(client: DiscordClient, dir = "") {
         helpCommandHelperCollection.get(command.category)?.commands.push({
           name: command.name,
           value: `${command.description}\n${
-            command.argsDescription ? `Args: ${command.argsDescription}\n` : ""
+            command.argsDescription ? `Args: ${command.argsDescription}\n` : ''
           }Cooldown: ${ms(command.cooldown)}`,
         });
       }
@@ -81,8 +81,8 @@ export async function registerCommands(client: DiscordClient, dir = "") {
         permissions: command.permissionsRequired,
         argsRequired: command.argsRequired,
       };
-      fs.writeFileSync(
-        "./all-commands.json",
+      await fs.writeFile(
+        './all-commands.json',
         JSON.stringify(allCommandsJSON, null, 2)
       );
 
@@ -93,14 +93,14 @@ export async function registerCommands(client: DiscordClient, dir = "") {
   }
 }
 
-export async function registerEvents(client: DiscordClient, dir = "") {
+export async function registerEvents(client: DiscordClient, dir = '') {
   const filePath = path.join(process.cwd(), dir);
   const files = await fs.readdir(filePath);
   for (const file of files) {
     const stat = await fs.lstat(path.join(filePath, file));
     if (stat.isDirectory()) registerEvents(client, path.join(dir, file));
-    if (file.endsWith("Event.js") || file.endsWith("Event.ts")) {
-      const { default: Event } = await import("../../" + path.join(dir, file));
+    if (file.endsWith('Event.js') || file.endsWith('Event.ts')) {
+      const { default: Event } = await import('../../' + path.join(dir, file));
       const event = new Event() as BaseEvent;
       client.events.set(event.name, event);
       client.on(event.name, event.run.bind(event, client));
