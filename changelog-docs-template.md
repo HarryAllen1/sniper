51c3b0f24c8921492ba634406de035af44dd290a
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-07-18 18:42:33 -0700

    better

--- changelog-docs-template.md
+++ changelog-docs-template.md
@@ -1,6 +1,6 @@
 # Changelog
 
-This page is really just the past 100 commits from the Github repo.
+This page is really just the past 50 commits from the Github repo.
 
 You can find a full list of commits on the [commits page](https://github.com/MajesticString/sniper/commits)
 
--- scripts/update-changelog.js
+++ scripts/update-changelog.js
@@ -1,17 +1,26 @@
 const { writeFile, readFile } = require('fs/promises');
 const { fetch } = require('undici');
 
-let stuffToAppend = '';
+/**
+ * Delay for a number of milliseconds
+ */
+function sleep(delay) {
+  var start = new Date().getTime();
+  while (new Date().getTime() < start + delay);
+}
 
-fetch(
-  'https://api.github.com/repos/MajesticString/sniper/commits?per_page=100'
-).then(async (res) => {
-  /**
-   * @type {import('./types.js').CommitRes[]}
-   */
-  const json = await res.json();
-  json.forEach(async (cmt) => {
-    stuffToAppend += `
+const main = async () => {
+  let stuffToAppend = '';
+
+  await fetch(
+    'https://api.github.com/repos/MajesticString/sniper/commits?per_page=50'
+  ).then(async (res) => {
+    /**
+     * @type {import('./types.js').CommitRes[]}
+     */
+    const json = await res.json();
+    json.forEach(async (cmt) => {
+      stuffToAppend += `
 ## ${new Date(cmt.commit.committer.date).toLocaleString()}
 
 ### Message
@@ -28,9 +37,14 @@ ${cmt.commit.message}
   ${await (await fetch(cmt.html_url + '.diff')).text()}
 </details>
 `;
+    });
+    await writeFile(
+      './docs/changelog.md',
+      (await readFile('./changelog-docs-template.md')) + stuffToAppend
+    );
   });
-  await writeFile(
-    './docs/changelog.md',
-    (await readFile('./changelog-docs-template.md')) + stuffToAppend
-  );
-});
+};
+
+sleep(2000);
+main();
+sleep(2000);
