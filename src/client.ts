9516cfa1d48860af4b4d624fc813e38b2d3c8a10
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-08-30 10:02:39 -0700

    nvm

--- src/client.ts
+++ src/client.ts
@@ -1,18 +1,21 @@
-import { SapphireClient } from '@sapphire/framework';
+import { container, SapphireClient } from '@sapphire/framework';
 import type { ClientOptions } from 'discord.js';
+import { cert, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { config } from './config.js';
 
 export class SniperClient extends SapphireClient {
   public constructor(options: ClientOptions) {
     super(options);
   }
 
-  // public initDB() {
-  //   initializeApp({
-  //     credential: cert(config.firebaseConfig),
-  //     projectId: 'discord-sniper-5c7f0',
-  //   });
-  //   container.db = getFirestore();
-  // }
+  public initDB() {
+    initializeApp({
+      credential: cert(config.firebaseConfig),
+      projectId: 'discord-sniper-5c7f0',
+    });
+    container.db = getFirestore();
+  }
 }
 
 declare module '@sapphire/pieces' {
